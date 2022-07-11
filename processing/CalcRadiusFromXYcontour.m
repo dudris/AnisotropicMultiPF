a0ee0c9a6a103a5cd1fd0e6e57bd0847b60c108d
dudris <martin.dudr@gmx.com>
2022-07-11 15:56:34 +0200

    cleaned code and updated help intro

--- processing/CalcRadiusFromXYcontour.m
+++ processing/CalcRadiusFromXYcontour.m
@@ -1,83 +1,48 @@
 %% r_interp = CalcRadiusFromXYcontour(XYcontour,th)
 % - Transfers contour matrix from Cartesian to polar coordinates and interpolates the radius
 % in values th. Assumes the contour is already centered.
+% - resulting contour to be plotted in polar coordinates (r_interp,th)
 % - Occurrence of NaNs from the interpolation at the angle jump is prevented
 % by extension of both curve ends by points from the other end.
 % INPUT
-% XYcontour - contour matrix with n points, size(XYcontour)=[n,2]
-% - contour to be a closed curve
+%   XYcontour - contour matrix with n points, size(XYcontour)=[n,2]
+%       - contour to be a closed curve
+%   th - polar angles (rad) for the 
 % OUTPUT
 % r_interp - radius interpolated in angles th
 
 function r_interp = CalcRadiusFromXYcontour(XYcontour,th)
-% XYcontour ... size(XYcontour) = [ptscount 2] = [ xcoord ycoord ]
+% __ XYcontour ... size(XYcontour) = [ptscount 2] = [ xcoord ycoord ]
     
     ang2 = atan2(XYcontour(:,2),XYcontour(:,1));
-%     plot(ang2,'o')
-%     r2 = sqrt(XYcontour(:,1).^2+XYcontour(:,2).^2);
+
     [~ , indsorted] = sort(ang2,'ascend');
-    % jump in angles removed from middle
+    % __ jump in angles removed from middle
     XYcontour = XYcontour(indsorted,:);
-    % add some more poits from back to the front and vice versa to avoid
-    % NaNs in interpolation
+    % __add some more poits from back to the front and vice versa to avoid NaNs in interpolation
     addpts = 10;
     numpts = size(XYcontour,1);
     ind_front_to_back = 1:addpts;
     ind_back_to_front = (numpts-addpts+1):numpts;
     ind_front = ind_front_to_back;
     ind_back = (numpts+addpts+1):(numpts+2*addpts);
-    % extended contour points
+    % __ extended contour points
     XYcontour_ext = zeros(numpts+2*addpts,2);
     XYcontour_ext(ind_front,:) = XYcontour(ind_back_to_front,:);
     XYcontour_ext(ind_back,:) = XYcontour(ind_front_to_back,:);
     XYcontour_ext(addpts+(1:numpts),:) = XYcontour;
-    
-%     plot(XYcontour(:,1),XYcontour(:,2),'.'), axis equal, hold on
-%     plot(XYcontour(ind_back_to_front,1),XYcontour(ind_back_to_front,2),'or')
-%     plot(XYcontour(ind_front_to_back,1),XYcontour(ind_front_to_back,2),'ob')
-%     plot(XYcontour_ext(ind_front,1),XYcontour_ext(ind_front,2),'xr')
-%     plot(XYcontour_ext(ind_back,1),XYcontour_ext(ind_back,2),'xb')
-    
+
     ang1 = atan2(XYcontour_ext(:,2),XYcontour_ext(:,1));
     r1 = sqrt(XYcontour_ext(:,1).^2+XYcontour_ext(:,2).^2);
     
-    % angles at front to be negative thanks to sorting before
+    % __ angles at front to be negative thanks to sorting before
     ang1(ind_front)= ang1(ind_front)-2*pi;
-    % angles at end to be positive thanks to sorting before
+    % __ angles at end to be positive thanks to sorting before
     ang1(ind_back)= ang1(ind_back)+2*pi;
-    
-%     plot(ang1,'o')
-    
-%     % move added points out of (-pi,pi)
-%     jumpinds = find(abs(diff(ang1))>6);
-%     if ~isempty(jumpinds) && length(jumpinds)==2
-%         front = 1:jumpinds(1);
-%         back = (jumpinds(2)+1):size(ang1,1);
-%         front_to_shift = ang1(front)<0;
-%         back_to_shift = ang1(back)>0;
-%         ang1(front(front_to_shift)) = ang1(front(front_to_shift)) + 2*pi;
-%         ang1(back(back_to_shift)) = ang1(back(back_to_shift)) - 2*pi;
-%     elseif length(jumpinds)==1
-%         front = 1:jumpinds;
-%         back = (jumpinds+1):size(ang1,1);
-%         if length(front)>length(back)
-%             back_to_shift = ang1(back)>0;
-%             ang1(back(back_to_shift)) = ang1(back(back_to_shift)) - 2*pi;
-%         elseif length(front)<length(back)
-%             front_to_shift = ang1(front)<0;
-%             ang1(front(front_to_shift)) = ang1(front(front_to_shift)) + 2*pi;
-%         else % length(front)==length(back)
-%             warning('CalcRadiusFromXYcontour: unexpected situation in prevention of NaNs in radius interpolation. ')
-%         end
-%     end
     
     [ang1, i1] = unique(ang1);
     r1 =r1(i1);
     
     r_interp(:,1) = interp1(ang1,r1,th,'linear');
-% plot(th,r_interp,'o')    
-%     plot(ang1,r1,'o')    
-%     plot(ang1,'o')    
-%     plot(diff(ang1),'o')    
     
 end
--- processing/calc_regularized_Wulff_normal_ang.m
+++ processing/calc_regularized_Wulff_normal_ang.m
@@ -1,39 +1,38 @@
 %%calc_regularized_Wulff_normal_ang
-% [th,th_mod, th_wulff] = calc_regularized_Wulff_normal_ang(phi,results_entry)
+% [phi,th_allwd, phi_rot] = calc_regularized_Wulff_normal_ang(phi,results_entry)
 % - to see the interval of allowed angles set in the script  'plot_allowed_angs=true'
 % INPUT
 %   phi ... linspace(-pi,pi) ... interval of POLAR angles for Wulff plotting
 %   results_entry ... results_entry.intf.params_incl_dep.(...)
 % OUTPUT
-%   phi        ... polar angles of sharp corner points added if present
-%   th_mod ... allowed INTERFACE NORMAL angles, rotated (to plot regularized Wulff shape)
+%   phi        ... as phi, only polar angles of sharp corner points added if present
+%   th_allwd ... allowed INTERFACE NORMAL angles, rotated (to plot regularized Wulff shape)
 %   phi_rot ... POLAR angles rotated by offset_ang (to plot rotated Wulff shape)
 % 
-function [phi,th_mod, phi_rot] = calc_regularized_Wulff_normal_ang(phi,results_entry)
+function [phi,th_allwd, phi_rot] = calc_regularized_Wulff_normal_ang(phi,results_entry)
 
     offset_ang = results_entry.intf.params_incl_dep.offset_ang;
 
     if results_entry.intf.params_incl_dep.Omega>1
-        % adds exact angles under which there should be the vortices and sorts ascendingly, if
+        % __ adds exact angles under which there should be the vortices and sorts ascendingly, if
         % they are present it does nothing
         phi = AddVorticesAngleCoord(results_entry.intf.params_incl_dep,phi); 
-%         th = unique(th);
-%         [th,ind_th_sort] = sort(th,'ascend');
+        
+        % __ interface normal angles 
         th_m = border_angles(results_entry.intf.params_incl_dep,false);
+        % __ apply rotation by offset_ang and bring all angles to (-pi,pi) interval
         th_m = rotate_to_first_inetrval(th_m,offset_ang);
-%         !!! 
+        
+        % __ get vector of allowed angles on the nfold Wulff edges of the same length as phi
         numel_per_int = floor(length(phi)/results_entry.intf.params_incl_dep.nfold);
         plot_allowed_angs = false;
-        [th_mod, ~]  = GetAllowedAngles(th_m,numel_per_int,plot_allowed_angs);
-%         th_for_forbb = linspace(-pi,pi,15e3)';
-%         cond_forb_ang = find_forbidden_angles(th_for_forbb,th_m,results_entry.intf.params_incl_dep);
-%         th_mod = th_for_forbb(~cond_forb_ang);
+        [th_allwd, ~]  = GetAllowedAngles(th_m,numel_per_int,plot_allowed_angs);
     else
-        th_mod = phi;
+        th_allwd = phi;
     end% if input.Omega>1
     
-    % rotate the wulff plot 
-        phi_rot = rotate_to_first_inetrval(phi,-offset_ang); % not sure why this takes the negative of offset ang
+    % __ rotate the wulff plot 
+        phi_rot = rotate_to_first_inetrval(phi,-offset_ang); 
 end
 
 %% AddVorticesAngleCoord
@@ -48,88 +47,3 @@ function th = AddVorticesAngleCoord(input,th)
     th = rotate_to_first_inetrval(th,0);
     th = sort(th,'ascend');
 end % func
-
-% %% GetAllowedAngles
-% % use linspace function to obtain allowed angles between the border points.
-% function [th_mod, ba_allwd]= GetAllowedAngles(border_angles,numpts,plotting)
-% 
-%     nfold = size(border_angles,1);
-% %     assert(mod(nfold,2)==0,'GetAllowedAngles<calc_regularized_Wulff_normal_ang not optimized for use with odd nfold')
-%     
-%     % makes sure  1st column is always smaller than 2nd
-%     ba_forb(:,1) = min(border_angles,[],2);
-%     ba_forb(:,2) = max(border_angles,[],2);
-%     
-% %     rearrange to identify interval of ALLOWED angles
-%     % the below fails when there should be jump in ALLOWED angles interval
-%     % then the rearranged still marks FORBIDDEN intervals
-%     ba_allwd = reshape(sort(ba_forb(:)),[2,nfold])';
-%     negcheck = all(sort(ba_allwd(:,1))==sort(ba_forb(:,1))); % true when failed
-% 
-%     if negcheck 
-%         % assume that allowed interval must have a jump
-%         clear ba_allwd
-%         ba_sorted = sort(ba_forb(:));
-%         % largest and smallest value form the interval with jump
-%         ba_allwd(1,[1,2]) = [max(ba_sorted), (min(ba_sorted)+2*pi) ];
-%         %the rest to be processed as before
-%         ba_temp = ba_sorted(2:(end-1));
-%         ba_allwd(2:nfold,:) = reshape(ba_temp,[2,(nfold-1)])';
-% %         ba_allwd = rotate_to_first_inetrval(ba_allwd,0);
-%         negcheck = all(sort(ba_allwd(:,1))==sort(ba_forb(:,1)));
-%         assert(~negcheck,'GetAllowedAngles in calc_regularized_Wulff_normal_ang failed')
-%     end
-%         
-%     % assumes 1st column is always smaller than 2nd
-%     int_width = diff(ba_allwd,[],2);
-%     jump_segment_ind = find((int_width-max(int_width))>pi);
-%     th_mod =  nan(nfold*numpts,1);
-% 
-%     indkk = 1:numpts;
-%     for kk = 1:nfold
-%         indkk = (1:numpts) + numpts*(kk-1);
-% %         disp(num2str(indkk))
-%         
-%         if isempty(jump_segment_ind) || (kk~=jump_segment_ind)
-%             th_mod(indkk,1) = linspace(ba_allwd(kk,1),ba_allwd(kk,2),numpts);
-%         else
-%             % segment with jump 
-%             negind = 1:floor(numpts/2);
-%             posind = (floor(numpts/2)+1):numpts;
-%             th_mod(indkk(negind),1) = linspace(ba_allwd(kk,1),-pi,numel(negind));
-%             th_mod(indkk(posind),1) = linspace(ba_allwd(kk,2),pi,numel(posind));
-%         end% if
-%     end% for
-%     
-%     th_mod = rotate_to_first_inetrval(th_mod,0);
-%     th_mod = unique(th_mod); % to throuw away duplicaets and sort
-%     
-% % %     the below samples the forbidden regions, not the allowed ones
-% % %     % assumes 1st column is always smaller than 2nd
-% %     jump_segment_ind = find(diff(border_angles,[],2)==max(diff(border_angles,[],2)));
-% %     th_mod =  nan(nfold*numpts,1);
-% % % 
-% %     for kk = 1:nfold
-% %         indkk = (1:numpts) + numpts*(kk-1);
-% % %         disp(num2str(indkk))
-% %         
-% %         if kk~=jump_segment_ind
-% %             th_mod(indkk,1) = linspace(border_angles(kk,1),border_angles(kk,2),numpts);
-% %         else
-% %             % segment with jump 
-% %             negind = 1:floor(numpts/2);
-% %             posind = (floor(numpts/2)+1):numpts;
-% %             th_mod(indkk(negind),1) = linspace(border_angles(kk,1),-pi,numel(negind));
-% %             th_mod(indkk(posind),1) = linspace(border_angles(kk,2),pi,numel(posind));
-% %         end% if
-% %     end% for
-% 
-%     if plotting
-%         figure(66)
-%         x = 1:numel(th_mod); plot(x,th_mod,'o',x(isnan(th_mod)),th_mod(isnan(th_mod)),'rx'), 
-%         polarplot(th_mod,ones(size(th_mod)),'.')
-%         title('allowed angles intervals')
-%     end
-% % sum(isnan(th_mod))
-% 
-% end 
